45be28061d78b1034df6314ee561a914a2aa72ac
programador.gdl@enginetsystems.com <programador.gdl@enginetsystems.com>
2021-01-11 18:05:21 -0600

    Finalizado para deploy

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -8,7 +8,7 @@ const Sidebar = () => {
             <h1>Konan <span>sma</span></h1>
             <NuevaMaleta/>
             <div className="productos">
-                <h2>Tus Maletas</h2>
+                <h2>Tus Grupos</h2>
                 <ListadoMaletas/>
             </div>
         </aside>
